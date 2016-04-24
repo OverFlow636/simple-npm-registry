5e3bb6d1b46e223d618acb38d58993a0a1534ac7
OverFlow636 <OverFlow636@gmail.com>
2016-04-24 03:58:40 -0400

    publishing works

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var request = require('request');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var crypto = require('crypto');
+var _ = require('lodash');
 
 // gather raw POST|PUT bodies
 app.use(function(req, res, next) {
@@ -18,6 +20,13 @@ app.use(function(req, res, next) {
   });
 });
 
+app.get('/favicon.ico', function(req, res) {
+  res.status(404).send();
+});
+
+
+
+
 // parse incomding auth info
 app.use(function(req, res, next) {
 
@@ -30,14 +39,72 @@ app.use(function(req, res, next) {
 
 
 // handle publishing of packages
+// check registry upstream to make sure the package does not exist?
 app.put('/:package', function(req, res) {
 
-  console.log('package put', req.headers, req.rawBody)
+  console.log('package put', req.headers)
+
+  var pkgData = JSON.parse(req.rawBody);
+
+  var files = Object.keys(pkgData._attachments);
+  files.forEach(function(fileName) {
+    var dir = './published/' + req.params.package;
+    fs.mkdir(dir, function(e, d) {
+
+      var file = dir + '/' + fileName;
+      fs.writeFileSync(file, new Buffer(pkgData._attachments[fileName].data, 'base64'));
+      delete pkgData._attachments[fileName];
+
+      var hash = crypto.createHash('sha1'),
+        stream = fs.createReadStream(file);
+
+      stream.on('data', function (data) {
+        hash.update(data, 'binary');
+      })
+
+      stream.on('end', function () {
+        console.log(hash.digest('hex'));
+      })
+
+    });
+  });
+
+  var packagejson = './published/' + req.params.package + '.json';
+  fs.readFile(packagejson, function(err, data) {
+    if (err) {
+      //first publish
+      fs.writeFileSync(packagejson, JSON.stringify(pkgData, null, 4));
+    } else {
+      //additional publish
+      fs.readFile(packagejson, function(err, data) {
+        var obj = JSON.parse(data);
+        var newObj = {};
+        _.merge(newObj, obj, pkgData);
+        fs.writeFileSync(packagejson, JSON.stringify(newObj, null, 4));
+      })
+    }
+  });
+
+
+
+
+
 
   res.send('asfd');
 })
 
+var adminInterfaceRouter = express.Router();
 
+adminInterfaceRouter.get('/', function(req, res) {
+
+  res.send('ok');
+});
+
+app.use('/snr', adminInterfaceRouter);
+
+app.get('/', function(req, res) {
+  res.redirect('/snr');
+});
 
 
 // User Auth   npm login | npm adduser
@@ -119,6 +186,23 @@ app.get('/_dl', function(req, res) {
 
 
 
+// Published packages downloads
+app.get('/:package/-/:filename', function(req, res) {
+
+  fs.readFile('./published/' + req.params.package + '/' + req.params.filename, function(err, data) {
+    if (err) {
+      console.log('no file')
+      res.status(404).send();
+    } else {
+      res.set('Content-Type', 'application/octet-stream');
+      res.set('Content-Disposition', 'attachment; filename=' + req.params.filename);
+
+      res.end(data, 'binary');
+    }
+  });
+
+});
+
 
 
 // should cache the json response and use it if the registry is ever down
@@ -126,28 +210,42 @@ app.get('/_dl', function(req, res) {
 // could filter all versions based on what files are available
 
 app.all('*', function (req, res) {
-  console.log('incoming', req.method, req.url, req.headers)
+  console.log('incoming', req.method, req.url)
 
-  request({
-    url: 'https://registry.npmjs.org' + req.url,
-    method: req.method
-  }, function(e, r, b) {
-    if (!e && r.statusCode === 200) {
+  fs.readFile('./published' + req.url + '.json', function(err, data) {
+    if (err) {
 
-      var data = JSON.parse(b);
-      var versions = Object.keys(data.versions);
-      versions.forEach(function(ver) {
-        data.versions[ver].dist.tarball = parseDist(data.versions[ver].dist);
-      })
+      // no local package, forward to npmjs
+      request({
+        url: 'https://registry.npmjs.org' + req.url,
+        method: req.method
+      }, function(e, r, b) {
+        if (!e && r.statusCode === 200) {
+          res.json(prepout(b));
+        } else {
+          res.status(r.statusCode).send(b);
+        }
+      });
 
-
-      res.json(data)
     } else {
-      console.error('error', e)
-      res.send('asdf')
-    }
 
-  });
+      // published modules do not need paths changed
+      res.set('content-type', 'text/html');
+      res.send(data);
+    }
+  })
+
+  function prepout(b) {
+    var data = JSON.parse(b);
+    var versions = Object.keys(data.versions);
+    versions.forEach(function(ver) {
+      data.versions[ver].dist.tarball = parseDist(data.versions[ver].dist);
+    });
+
+    return data;
+  }
+
+
 });
 
 
