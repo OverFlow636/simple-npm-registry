a446425112e22e236cc535dc4721ecbbf3f22d53
OverFlow636 <OverFlow636@gmail.com>
2016-04-24 11:23:20 -0400

    user registration/login and package publishing

--- index.js
+++ index.js
@@ -6,32 +6,47 @@ var bodyParser = require('body-parser');
 var crypto = require('crypto');
 var _ = require('lodash');
 
+var defaultSettings = {
+  users: []
+};
+var settings;
+fs.readFile('./settings.json', function (err, data) {
+  if (err) {
+    settings = defaultSettings;
+  } else {
+    settings = _.defaults(JSON.parse(data), defaultSettings);
+  }
+});
+
+
 // gather raw POST|PUT bodies
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   req.rawBody = '';
   req.setEncoding('utf8');
 
-  req.on('data', function(chunk) {
+  req.on('data', function (chunk) {
     req.rawBody += chunk;
   });
 
-  req.on('end', function() {
+  req.on('end', function () {
     next();
   });
 });
 
-app.get('/favicon.ico', function(req, res) {
+app.get('/favicon.ico', function (req, res) {
   res.status(404).send();
 });
 
 
-
-
-// parse incomding auth info
-app.use(function(req, res, next) {
-
+// parse incomding auth info assign req.user = the authed user
+app.use(function (req, res, next) {
   if (req.headers.authorization) {
-    console.log('incoming auth: ', req.headers.authorization);
+    var token = req.headers.authorization.split(' ')[1];
+    settings.users.forEach(function (user) {
+      if (user.token === token) {
+        req.user = user;
+      }
+    })
   }
 
   next();
@@ -40,169 +55,180 @@ app.use(function(req, res, next) {
 
 // handle publishing of packages
 // check registry upstream to make sure the package does not exist?
-app.put('/:package', function(req, res) {
-
-  console.log('package put', req.headers)
-
+app.put('/:package', function (req, res) {
   var pkgData = JSON.parse(req.rawBody);
 
+  // create all the files posted to us
   var files = Object.keys(pkgData._attachments);
-  files.forEach(function(fileName) {
+  files.forEach(function (fileName) {
+    // make a directory under published for the package name
     var dir = './published/' + req.params.package;
-    fs.mkdir(dir, function(e, d) {
+    fs.mkdir(dir, function (e, d) {
 
       var file = dir + '/' + fileName;
       fs.writeFileSync(file, new Buffer(pkgData._attachments[fileName].data, 'base64'));
       delete pkgData._attachments[fileName];
-
-      var hash = crypto.createHash('sha1'),
-        stream = fs.createReadStream(file);
-
-      stream.on('data', function (data) {
-        hash.update(data, 'binary');
-      })
-
-      stream.on('end', function () {
-        console.log(hash.digest('hex'));
-      })
-
     });
   });
 
+  // create the record for the published package
   var packagejson = './published/' + req.params.package + '.json';
-  fs.readFile(packagejson, function(err, data) {
+  fs.readFile(packagejson, function (err, data) {
     if (err) {
       //first publish
       fs.writeFileSync(packagejson, JSON.stringify(pkgData, null, 4));
     } else {
       //additional publish
-      fs.readFile(packagejson, function(err, data) {
+      fs.readFile(packagejson, function (err, data) {
         var obj = JSON.parse(data);
         var newObj = {};
         _.merge(newObj, obj, pkgData);
         fs.writeFileSync(packagejson, JSON.stringify(newObj, null, 4));
       })
     }
+
+    res.status(201).json({ok: 'created or updated'});
   });
 
-
-
-
-
-
-  res.send('asfd');
-})
+});
 
 var adminInterfaceRouter = express.Router();
 
-adminInterfaceRouter.get('/', function(req, res) {
+adminInterfaceRouter.get('/', function (req, res) {
 
   res.send('ok');
 });
 
 app.use('/snr', adminInterfaceRouter);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.redirect('/snr');
 });
 
 
 // User Auth   npm login | npm adduser
-app.put('/-/user/org.couchdb.user:*', function(req, res) {
+app.put('/-/user/org.couchdb.user:*', function (req, res) {
+  var data = JSON.parse(req.rawBody);
+  data.password = crypto.createHash('sha1').update(data.password).digest('hex');
 
-  console.log('user put', req.headers, req.rawBody);
+  var found = false;
+  settings.users.forEach(function (user) {
+    if (user.name === data.name && user.email === data.email) {
+      found = true;
 
-  //failed login = {"ok":false,"error":"Unknown error while authenticating"}
+      if (user.password === data.password) {
+        output(user);
+      } else {
+        res.status(401).json({"ok": false, "id": "org.couchdb.user:undefined"});
+      }
 
-  // wrong user/pass {"ok":false,"id":"org.couchdb.user:undefined"} 401 
+      return FALSE;
+    }
+  });
 
-  // createed user = 201
-  // {"ok":true,"id":"org.couchdb.user:undefined","rev":"_we_dont_use_revs_any_more","token":"b0ac7745-77f4-4030-8b84-200e6495d46c"}
-  
+  if (!found) {
+    var user = {
+      name: data.name,
+      email: data.email,
+      password: data.password
+    };
+    user.token = crypto.createHash('sha1').update(JSON.stringify(user)).digest('hex');
+    settings.users.push(user);
+    saveSettings();
 
-  // store user name and pw , then issue a token for later lookup of whos token it is.
+    output(user);
+  }
 
+  function output(user) {
+    res.status(201).json({
+      ok: true,
+      id: "org.couchdb.user:undefined",
+      rev: "_we_dont_use_revs_any_more",
+      token: user.token
+    });
+  }
 
-
-  request({
-    url: 'https://registry.npmjs.org' + req.url,
-    method: 'PUT',
-    body: req.rawBody,
-    headers: {
+  /*
+   request({
+     url: 'https://registry.npmjs.org' + req.url,
+     method: 'PUT',
+     body: req.rawBody,
+     headers: {
       'content-type': 'application/json'
-    }
-  }, function(e, r, b) {
+     }
+   }, function(e, r, b) {
 
-    if (e) {
-      console.log('put user error', e)
+   if (e) {
+     console.log('put user error', e)
 
-      res.send('asf')
-    } else {
+     res.send('asf')
+   } else {
 
-      console.log('auth looks like ', b, r.statusCode)
+     console.log('auth looks like ', b, r.statusCode)
 
 
-      res.status(r.statusCode).send(b);
-    }
+     res.status(r.statusCode).send(b);
+   }
 
-  })
+   })*/
 
 })
 
+// save the settings object back to its file
+function saveSettings() {
+  fs.writeFileSync('./settings.json', JSON.stringify(settings, null, 4));
+}
+
 
 // File Download Proxy
-app.get('/_dl', function(req, res) {
+app.get('/_dl', function (req, res) {
   var url = new Buffer(req.query.path, 'base64').toString('ascii');
   var filename = url.substr(url.lastIndexOf('/') + 1);
 
   console.log('Downloading: ', url)
 
-  fs.readFile('cache/' + req.query.sha, function(err, data) {
+  fs.readFile('cache/' + req.query.sha, function (err, data) {
     if (err) {
+      // cache for file does not exist, grab the remote file and, cache it, and return it
       request({
-        url: url,
-        method: req.method,
+        url     : url,
+        method  : req.method,
         encoding: null
       }, function (e, r, b) {
         if (!e && r.statusCode === 200) {
-          res.set('Content-Type', 'application/octet-stream');
-          res.set('Content-Disposition', 'attachment; filename=' + filename);
-
           fs.writeFileSync('cache/' + req.query.sha, b);
 
-          res.end(b, 'binary');
+          sendFile(b, filename);
         } else {
           res.status(404).send('');
         }
       });
     } else {
-      res.set('Content-Type', 'application/octet-stream');
-      res.set('Content-Disposition', 'attachment; filename=' + filename);
-
-      res.end(data, 'binary');
+      //return cached file
+      sendFile(data, filename)
     }
   });
 });
 
+function sendFile(file, filename) {
+  res.set('Content-Type', 'application/octet-stream');
+  res.set('Content-Disposition', 'attachment; filename=' + filename);
 
+  res.end(file, 'binary');
+}
 
-// Published packages downloads
-app.get('/:package/-/:filename', function(req, res) {
-
-  fs.readFile('./published/' + req.params.package + '/' + req.params.filename, function(err, data) {
+// Downloads for published packages
+app.get('/:package/-/:filename', function (req, res) {
+  fs.readFile('./published/' + req.params.package + '/' + req.params.filename, function (err, data) {
     if (err) {
-      console.log('no file')
+      console.log('no file');
       res.status(404).send();
     } else {
-      res.set('Content-Type', 'application/octet-stream');
-      res.set('Content-Disposition', 'attachment; filename=' + req.params.filename);
-
-      res.end(data, 'binary');
+      sendFile(data, req.params.filename);
     }
   });
 
 });
-
 
 
 // should cache the json response and use it if the registry is ever down
@@ -212,18 +238,27 @@ app.get('/:package/-/:filename', function(req, res) {
 app.all('*', function (req, res) {
   console.log('incoming', req.method, req.url)
 
-  fs.readFile('./published' + req.url + '.json', function(err, data) {
+  fs.readFile('./published' + req.url + '.json', function (err, data) {
     if (err) {
 
       // no local package, forward to npmjs
       request({
         url: 'https://registry.npmjs.org' + req.url,
         method: req.method
-      }, function(e, r, b) {
+      }, function (e, r, b) {
         if (!e && r.statusCode === 200) {
-          res.json(prepout(b));
+          b = prepout(b);
+          fs.writeFileSync('./cache' + req.url + '.json', JSON.stringify(b, null, 4));
+          res.json(b);
         } else {
-          res.status(r.statusCode).send(b);
+          // try to send cached output
+          fs.readFile('./cache' + req.url + '.json', function(err, data) {
+            if (err) {
+              res.status(r.statusCode).send(b);
+            } else {
+              res.status(200).send(data);
+            }
+          });
         }
       });
 
@@ -233,12 +268,12 @@ app.all('*', function (req, res) {
       res.set('content-type', 'text/html');
       res.send(data);
     }
-  })
+  });
 
   function prepout(b) {
     var data = JSON.parse(b);
     var versions = Object.keys(data.versions);
-    versions.forEach(function(ver) {
+    versions.forEach(function (ver) {
       data.versions[ver].dist.tarball = parseDist(data.versions[ver].dist);
     });
 
@@ -254,7 +289,6 @@ function parseDist(dist) {
 
   return 'http://localhost/_dl?path=' + path + '&sha=' + dist.shasum
 }
-
 
 
 app.listen(80, function () {
